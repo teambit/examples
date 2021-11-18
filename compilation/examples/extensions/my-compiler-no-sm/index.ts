a4b0c6d282194ce46eb6a663f48cc4b9490e7b25
Eden <eden@bit.dev>
2021-11-18 04:19:28 +0200

    remove my-comiler-no-sm

--- compilation/examples/extensions/my-compiler-no-sm/index.ts
+++
@@ -1,5 +0,0 @@
-import { MyCompilerNoSmAspect } from './my-compiler-no-sm.aspect';
-
-export { MyCompilerNoSmMain } from './my-compiler-no-sm.main.runtime';
-export default MyCompilerNoSmAspect;
-export { MyCompilerNoSmAspect };
--- compilation/examples/extensions/my-compiler-no-sm/my-compiler-no-sm.aspect.ts
+++
@@ -1,6 +0,0 @@
-import { Aspect } from '@teambit/harmony';
-
-export const MyCompilerNoSmAspect = Aspect.create({
-  id: 'compilation/examples/extensions/my-compiler-no-sm',
-});
-  
