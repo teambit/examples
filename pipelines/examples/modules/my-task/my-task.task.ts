744cf9afe6bba62d24ff792b803a261eedaed0f9
Eden <eden@bit.dev>
2021-12-29 01:48:17 +0200

    fix build task typos

--- pipelines/examples/modules/my-task/my-task.task.ts
+++ pipelines/examples/modules/my-task/my-task.task.ts
@@ -12,13 +12,13 @@ export class MyTask implements BuildTask {
   readonly name = 'MyTask';
 
   async execute(context: BuildContext): Promise<BuiltTaskResult> {
-    // Prepare the component results array which will be used to report back the components proccessed
+    // Prepare the component results array which will be used to report back the components processed
     // as well as any additional data regarding this build task execution
     const componentsResults: ComponentResult[] = [];
     // In most cases, only the 'seeder capsules' are the ones relevant for your build
     const capsules = context.capsuleNetwork.seedersCapsules;
     capsules.forEach((capsule) => {
-      // Preprate an 'errors' array to report back of any erros during execution (this will be part of the 'Component Results' data)
+      // Prepare an 'errors' array to report back of any errors during execution (this will be part of the 'Component Results' data)
       const errors: Error[] = [];
       // Each 'capsule' provides data regarding the component as well as the capsule itself
       const componentName = capsule.component.id.name;
@@ -27,7 +27,7 @@ export class MyTask implements BuildTask {
       const artifactContent = `The component's name is: ${componentName}`;
 
       try {
-        // Generate the artifact inside the capsule's diretory
+        // Generate the artifact inside the capsule's directory
         fs.writeFileSync(
           path.join(capsuleDir, 'output.my-artifact.txt'),
           artifactContent
@@ -43,7 +43,7 @@ export class MyTask implements BuildTask {
         {
           generatedBy: this.aspectId,
           name: this.name,
-          // The glob pattern for artifacts to include in the comoponent version
+          // The glob pattern for artifacts to include in the component version
           globPatterns: [`**/*.my-artifact.txt`],
         },
       ],
